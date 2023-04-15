4844e0a3f9c883a60f644efc9d40672b34a0242d
Prasanta Acharyya <acharyya.prasanta05@gmail.com>
2023-04-15 12:16:25 +0530

    NavBar add in Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Image from '../components/error.jpeg'
+import NavBar from '../components/NavBar'
 
 //Error page with an image
 const Error = () => {
   return (
     <div>
         <div >
+          <NavBar />
         <img src={Image} className="object-cover w-full h-fit" alt="Error Page" />
         </div>
     </div>
